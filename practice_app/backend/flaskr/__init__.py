67cf22510a9ae63c966fc862b7faa31cd4b2e7a3
Murat Ekici <44344210+muratekici@users.noreply.github.com>
2020-05-25 11:29:54 +0300

    home page added

--- practice_app/backend/flaskr/__init__.py
+++ practice_app/backend/flaskr/__init__.py
@@ -6,14 +6,19 @@ Created on Mon May 11 13:21:17 2020
 """
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 from . import db
+from . import routes_product
+from . import routes_createproduct
+from . import exchange_rate_api as currency
+
 
 def create_app(test_config = None):
     
     app = Flask(__name__, instance_relative_config=True)
-    
-    
+    app.register_blueprint(routes_product.product_bp)
+    app.register_blueprint(routes_createproduct.createproduct_bp)
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -34,7 +39,32 @@ def create_app(test_config = None):
     
     @app.route('/')
     def hello():
-        return 'Welcome to Tursu!'
+        
+        products = db.query_db('select * from Product')
+        product_list = []
+        for product in products:
+            
+            prices = currency.prices_in_currencies(product["price"])
+
+            price_try = round(prices["TRY"], 1)
+            price_usd = round(prices["USD"], 1)
+            price_eur = round(prices["EUR"], 1) 
+
+            product_dict = {"id": product["id"],
+                            "name": product["name"],
+                            "price": {
+                                "try": price_try,
+                                "usd": price_usd,
+                                "eur": price_eur
+                            },
+                            "seller": product["seller"],
+                            "description": product["description"],
+                            "location": product["location"],
+                            "url": product["url"]
+                            }
+            product_list.append(product_dict)
+
+        return render_template("home.html", products=product_list)
     
     
     db.init_app(app)
