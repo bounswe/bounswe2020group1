611fbdcd63f202d3d1fe5719062401e1e41e01ba
Murat Ekici <44344210+muratekici@users.noreply.github.com>
2020-05-25 11:48:14 +0300

    Deleted redundant function, minor fixes

--- practice_app/backend/flaskr/routes_createproduct.py
+++ practice_app/backend/flaskr/routes_createproduct.py
@@ -3,18 +3,13 @@
 routes_addproduct.py
 """
 
-from flask import Blueprint, abort, jsonify, request, render_template
+from flask import redirect, Blueprint, abort, jsonify, request, render_template
 from . import db
 from . import bad_word_filter
 from . import location_api
 
 createproduct_bp = Blueprint('Create Product Routes', __name__)
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
 
 @createproduct_bp.route('/createproduct/', methods=['POST', 'GET'])
 def createproduct():
@@ -48,7 +43,7 @@ def createproduct():
 		cur.execute("insert into Product (name, seller, price, description, location, url) values(?, ?, ?, ?, ?, ?)", (name, seller, price, description, location, image_url))
 		cur.commit()
 
-		return "Product is succesfully added"
+		return redirect("/")
 
 	return render_template("create_product.html")
 
