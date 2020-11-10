574314b39d3d758172c3fd60267518a15344cf56
busekabakoglu <buse.kabakoglu@boun.edu.tr>
2020-11-10 12:00:59 +0300

    password hide and small edits

--- Android/TursuApp/app/src/main/java/com/example/tursuapp/LoginActivity.kt
+++ Android/TursuApp/app/src/main/java/com/example/tursuapp/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.tursuapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
