24332106786680c66c468e4f4b0468a7e4290670
AsenaKarolin <asenaozdemirkabatas@gmail.com>
2020-11-16 02:18:39 +0300

    Initial commit for Sign in, Sign up and Forgot Password pages.

---
+++ Frontend/tursu/src/ForgotPassword.js
@@ -0,0 +1,47 @@
+import React, { Component} from 'react';
+import "./sign_components.css";
+import logo from './rsz_11logo.png';
+
+export default class ForgotPassword extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.goToLogin = this.goToLogin.bind(this);
+    }
+    handleSubmit(event){
+        console.log("form submitted");
+        console.log(this.state.email);
+        //API request goes here
+        event.preventDefault();
+    }
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    goToLogin(){
+        this.props.onLoginChange();
+    }
+    render() {
+        return(
+            <div className="login">
+                <img src={logo} alt="Tursu Logo"></img>
+                <h1>Reset Password</h1>
+                <form onSubmit={this.handleSubmit}>
+
+                    <input className="tursu_input" type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+                    <br/>
+                    <br/>
+                    <button type="submit" className="tursu_button">Reset Password</button>
+                </form>
+                    <button type="button" onClick={this.goToLogin} className="smallButton">Remembered the password? Sign in.</button>
+            </div>
+        )
+    }
+}
---
+++ Frontend/tursu/src/customer_registration.js
@@ -0,0 +1,58 @@
+import React, { Component} from 'react';
+import "./sign_components.css";
+import logo from './rsz_11logo.png';
+
+export default class Customer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: "",
+            password_confirmation: "",
+            user_type: "customer",
+            name: "",
+            surname: "",
+            username: ""
+            }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleSubmit(event){
+        console.log("form submitted");
+        //password confirmation goes here
+        //API request goes here
+        event.preventDefault();
+    }
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    render() {
+        return(
+            <div className="registration">
+
+                <form onSubmit={this.handleSubmit}>
+                    <input className="tursu_input" type="text" name="name" id="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} required />
+                    <input className="tursu_input" type="text" name="surname" id="surname" placeholder="Surname" value={this.state.surname} onChange={this.handleChange} required />
+                    <br/>
+                    <input className="tursu_input" type="text" name="username" id="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} required />
+
+                    <input className="tursu_input" type="email" name="email" id="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+                    <br/>
+
+                    <input className="tursu_input" type="password" name="password" id="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+
+
+                    <input className="tursu_input" type="password" name="password_confirmation" id="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
+                    <br/>
+
+                    <button type="submit" className="tursu_button">Sign Up</button>
+                </form>
+
+            </div>
+        )
+    }
+}
