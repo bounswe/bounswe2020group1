9d64afb7d635e2b7073ab7e1edb37dd3702208d9
Barış ALHAN <barisalhan003@gmail.com>
2020-11-18 20:48:09 +0300

    Dependencies are installed.

--- Frontend/tursu/src/HomeGrid.js
+++ Frontend/tursu/src/HomeGrid.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-//import Navbar from './HomePage';
-//import Product from './HomePage';
+import Navbar from './HomePage';
+import Product from './HomePage';
 import Stepper from "./Stepper";
 
 //const { Navbar, Product } = require('./HomePage');
