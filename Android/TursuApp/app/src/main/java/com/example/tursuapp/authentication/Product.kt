0d505199cb131a9324f0e18e0fb33719edc70e37
busekabakoglu <buse.kabakoglu@boun.edu.tr>
2020-11-20 01:09:54 +0300

    dummy gridview added

---
+++ Android/TursuApp/app/src/main/java/com/example/tursuapp/authentication/Product.kt
@@ -0,0 +1,11 @@
+package com.example.tursuapp.authentication
+
+class Product {
+    var name: String? = null
+    var image: Int? = null
+
+    constructor(name: String, image: Int) {
+        this.name = name
+        this.image = image
+    }
+}
