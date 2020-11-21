2f4994dd6debbefd74dcce80567df79a3b1a71d9
memetc <33198811+memetc@users.noreply.github.com>
2020-11-21 21:12:48 +0300

    Update App.js

--- Frontend/tursu/src/App.js
+++ Frontend/tursu/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import ProductDetailPage from "./ProductDetailPage";
+import HomePage from "./HomePage";
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Category from "./Category";
 
 function App() {
-    return (
+  return (
+      <Router>
         <div className="App">
-            <ProductDetailPage />
+            <Route path="/" exact component={HomePage} />
+            <Route path="/categories/:category" exact component={Category} />
         </div>
-    );
+      </Router>
+  );
 }
 
 export default App;
