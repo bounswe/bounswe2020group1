5d2a525b73b277400f79eaca1cbcbe4b8f124f25
onurkilic2301 <onurkilic2301@gmail.com>
2020-12-02 15:40:42 +0300

    Updated search endpoint
    Updated search endpoint to support vendor search, filtering and sorting

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,6 +1,91 @@
+""" Search endpoint and helper functions """
+import string
 from django.http import HttpResponse, JsonResponse
 from django.db.models import Q
+import nltk
+from nltk.corpus import stopwords
+from nltk import word_tokenize
 from product.models import Product, Image
+from registered_user.models import Vendor
+from filter_sort.utils import product_filter, product_sort
+
+nltk.download("punkt")
+nltk.download("stopwords")
+
+class SearchHelper:
+    """ Helper class for search functionalities """
+    @staticmethod
+    def keyword_extractor(query):
+        """ Keyword extractor function with stopword and punctuation removal """
+        stop = set(stopwords.words('english') + list(string.punctuation))
+        words = word_tokenize(query)
+        keywords = [word for word in words if len(word)>1 and word not in stop]
+        return keywords
+
+    @staticmethod
+    def product_search(search_string):
+        """ Searching function to search products with a string """
+        keywords = SearchHelper.keyword_extractor(search_string)
+        if len(keywords) == 0:
+            data = Product.objects.none()
+        else:
+            query = Q()
+            for word in keywords:
+                query |= Q(name__icontains=word) | \
+                        Q(description__icontains=word) | \
+                        Q(category__name__icontains=word) | \
+                        Q(vendor__user__user__first_name__icontains=word)
+            data = Product.objects.filter(query)
+        return data
+
+    @staticmethod
+    def prepare_products(data):
+        """ Function to prepare the response for product search """
+        products = []
+        static_url = "http://3.232.20.250/static/" # TODO Move this to conf
+        for product in data:
+            images = Image.objects.filter(product=product)
+            if len(images) > 0:
+                photo_url = f"{static_url}{images[0].photo}"
+            else:
+                photo_url = ""
+            product_info ={"id": product.pk,
+                           "name": product.name,
+                           "photo_url": photo_url,
+                           "vendor_name": product.vendor.user.user.first_name,
+                           "category": product.category.name,
+                           "rating": product.rating,
+                           "stock": product.stock,
+                           "price": product.price
+                        }
+            products.append(product_info)
+        return products
+
+    @staticmethod
+    def vendor_search(search_string):
+        """ Searching function to search vendors with a string """
+        keywords = SearchHelper.keyword_extractor(search_string)
+        if len(keywords) == 0:
+            data = Vendor.objects.none()
+        query = Q()
+        for word in keywords:
+            query |= Q(user__user__first_name__icontains=word)
+        data = Vendor.objects.filter(query)
+        vendors = []
+        for vendor in data:
+            if vendor.location:
+                location = f"Latitude: {vendor.location.latitude}, " \
+                           f"Longitude: {vendor.location.longitude}"
+            else:
+                location = ""
+            vendor_info ={"name": vendor.user.user.first_name,
+                          "location": location,
+                          "is_verified": vendor.is_verified,
+                          "rating": vendor.rating
+                        }
+            vendors.append(vendor_info)
+        return vendors
+
 
 def index(request):
     """Returns searched products when GET request is made.
@@ -21,42 +106,20 @@ def index(request):
     """
     try:
         search_string = request.GET["search_string"]
-    except :
+    except KeyError:
         return JsonResponse([], safe=False)
     try:
         search_type = request.GET["search_type"]
-    except :
+    except KeyError:
         return HttpResponse('Select search_type as vendor or product to search',status=400)
-
     if search_type == "product":
-        query = Q()
-        for word in search_string.split():
-            query |= Q(name__icontains=word) | \
-                    Q(description__icontains=word) | \
-                    Q(category__name__icontains=word) | \
-                    Q(vendor__user__user__first_name__icontains=word)
-        data = Product.objects.filter(query)
+        searched_products = SearchHelper.product_search(search_string)
+        filtered_products = product_filter(request, searched_products)
+        sorted_products = product_sort(request, filtered_products)
+        products =  SearchHelper.prepare_products(sorted_products)
+        return JsonResponse(products, safe=False)
     elif search_type == "vendor":
-        raise NotImplementedError("Vendor search is not yet implemented")
+        vendors = SearchHelper.vendor_search(search_string)
+        return JsonResponse(vendors, safe=False)
     else:
         return HttpResponse('Select search_type as vendor or product to search',status=400)
-
-    products = []
-    static_url = "http://3.232.20.250/static/" # TODO Move this to conf
-    for product in data:
-        images = Image.objects.filter(product=product)
-        if(len(images) > 0):
-            photo_url = f"{static_url}{images[0].photo}"
-        else:
-            photo_url = ""
-        product_info ={"id": product.pk,
-                       "name": product.name,
-                       "photo_url": photo_url,
-                       "vendor_name": product.vendor.user.user.first_name,
-                       "category": product.category.name,
-                       "rating": product.rating,
-                       "stock": product.stock,
-                       "price": product.price
-                    }
-        products.append(product_info)
-    return JsonResponse(products, safe=False)
