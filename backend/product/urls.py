06490ca466787e4ebb74a197679b49df4f0a090b
onurkilic2301 <onurkilic2301@gmail.com>
2020-12-05 15:12:58 +0300

    Added add product feature

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('category/', views.category, name='category'),
+    path('add/', views.add_product, name='add_product'),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+"""Views related to product"""
+from django.http import JsonResponse, HttpResponse
 from django.db.models import Q
+from django import forms
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from product.models import Product, Category, Image
+from registered_user.models import get_vendor_from_request
+
 
 
 def category(request):
@@ -54,3 +60,55 @@ def category(request):
         products.append(product_info)
 
     return JsonResponse(products, safe=False)
+
+class ImageUploadForm(forms.Form):
+    """Image upload form."""
+    photo = forms.ImageField()
+
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes((IsAuthenticated,))
+@api_view(['POST'])
+def add_product(request):
+    """Adds product with given parameters when POST request is made."""
+    vendor = get_vendor_from_request(request)
+    if(vendor is None):
+        return HttpResponse("Vendor authentication failed", status=401)
+    try:
+        category = Category.objects.get(name=request.POST["category"])
+    except Exception:
+        return HttpResponse("Invalid category", status=400)
+    try:
+        name = request.POST["name"]
+        brand = request.POST["brand"]
+        description = request.POST["description"]
+    except KeyError:
+        return HttpResponse("Name, brand or description not given.", status=400)
+    rating = -1
+    try:
+        stock = int(request.POST["stock"])
+        price = float(request.POST["price"])
+    except (KeyError, ValueError):
+        return HttpResponse("Stock or price Invalid", status=400)
+    try:
+        product = Product.objects.create(
+            vendor=vendor,
+            category=category,
+            name=name,
+            description=description,
+            rating=rating,
+            stock=stock,
+            price=price,
+            brand=brand
+            )
+        product.save()
+    except Exception:
+        return HttpResponse("Product couldn't be created with current params", status=400)
+    form = ImageUploadForm(request.POST, request.FILES)
+    try:
+        if form.is_valid():
+            image = Image.objects.create(product=product)
+            image.photo = form.cleaned_data["photo"]
+            image.save()
+    except Exception:
+        pass
+    return HttpResponse("success")
