26083222a0541413ee76b63505cab4507178ddb2
AsenaKarolin <asenaozdemirkabatas@gmail.com>
2020-12-20 18:54:54 +0300

    My Information page for customers is created with mock data.

--- Frontend/tursu/src/Profiles/MyInfo.js
+++ Frontend/tursu/src/Profiles/MyInfo.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import Grid from "@material-ui/core/Grid";
+import InfoBox from "./InfoBox"
 
 
 export default class myInfo extends Component {
@@ -11,15 +12,33 @@ export default class myInfo extends Component {
     }
 
     render() {
-            return(
-                <Grid item>
-                    <div>
-                        <a>My Info</a> <br/>
-                        <a>My Info</a> <br/>
-                        <a>My Info</a> <br/><a>My Info</a> <br/><a>My Info</a> <br/><a>My Info</a> <br/><a>My Info</a> <br/><a>My Info</a> <br/>
-
-                    </div>
-                </Grid>
-            )
+        const fields = [
+        {key: 'Name',
+            value: 'April',
+            editable : 'True'},
+        {key: 'Surname',
+            value: 'Ludgate',
+            editable : 'True'},
+        {key: 'Username',
+            value: 'April04',
+            editable : 'True'},
+        {key: 'Email',
+            value: 'april.ludgate@mail.com',
+            editable : 'False'},
+        {key: 'Password',
+            value: '****',
+            editable : 'True'}
+        ];
+        return(
+            <Grid item>
+                <div>
+                    <InfoBox field={fields[0]}/>
+                    <InfoBox field={fields[1]}/>
+                    <InfoBox field={fields[2]}/>
+                    <InfoBox field={fields[3]}/>
+                    <InfoBox field={fields[4]}/>
+                </div>
+            </Grid>
+        )
     }
 }
