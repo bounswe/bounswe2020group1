ef96febc8a05540b7fb68f423fd69c46ecbb5ed3
memetc <33198811+memetc@users.noreply.github.com>
2020-12-21 02:32:47 +0300

    Resolve Conflict in App.js

--- Frontend/tursu/src/App.js
+++ Frontend/tursu/src/App.js
@@ -17,8 +17,8 @@ function App() {
                 <Route path="/" exact component={HomePage} />
                 <Route path="/categories/:category" exact component={Category} />
                 <Route path="/signIn" exact component={Sign} />
-                <Route path="/product" exact component={ProductDetailPage} />
-                <Route path="/search" exact component={SearchPage} />
+                <Route path="/product/:id"  component={ProductDetailPage} />
+                <Route path="/search/:search_string/:search_type/:fprice_lower?/:fprice_upper?/:fvendor_name?/:fcategory?/:sort_by?"  component={SearchPage} />
                 <Route path="/vendorProfile" exact component={VendorProfilePage} />
                 <Route path="/customerProfile" exact component={CustomerProfilePage} />
             </div>
