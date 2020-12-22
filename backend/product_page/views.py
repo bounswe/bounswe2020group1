44a6feef48d172e1490abc65975054e4dc4b7833
16omerak <44061298+16omerak@users.noreply.github.com>
2020-12-22 22:46:51 +0300

    Comments added

--- backend/product_page/views.py
+++ backend/product_page/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.db.models import Q
 from product.models import Product, Image
+from comment.models import Comment
 
 def index(request):
     try:
@@ -21,16 +22,28 @@ def index(request):
         photo_url = f"{static_url}{images[0].photo}"
     else:
         photo_url = ""
+        
+    comments = Comment.objects.filter(Q(prouct=product))
+    comments_of_product = []
+    if comments != None:
+        for comment in comments:
+            comment_info = {"id": comment.pk,
+                "customer": comment.customer.user.user.username,
+                "text": comment.text,
+                "rating": comment.rating
+            }
+    
+    
     product_info = {"id": product.pk,
                 "name": product.name,
                 "description": product.description,
-                "photo_url": photo_url,    #TODO ADD PHOTO URL
+                "photo_url": photo_url,
                 "vendor_name": product.vendor.user.user.first_name,
                 "category": product.category.name,
                 "rating": product.rating,
                 "stock": product.stock,
                 "price": product.price,
-                "comments": []      #WILL BE EMPTY FOR THE FIRST MILESTONE
+                "comments": comments_of_product
             }
                    
     return JsonResponse(product_info, safe=False)
