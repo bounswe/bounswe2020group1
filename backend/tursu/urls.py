fe31e55d7c2c90a1ed4f56514de89856db28f5bd
Onur Kılıçoğlu <onurkilic2301@gmail.com>
2020-12-23 08:29:15 +0300

    Comment urls added

--- backend/tursu/urls.py
+++ backend/tursu/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('shoppinglist/', include('shopping_list.urls')),
     path('order/', include('order.urls')),
+    path('comment/', include('comment.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
