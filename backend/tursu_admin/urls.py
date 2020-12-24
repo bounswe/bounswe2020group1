cc5fe1c44a177be221954340b4cfb2ee162f7013
onurkilic2301 <onurkilic2301@gmail.com>
2020-12-24 23:30:35 +0300

    Corrected url path function binding

--- backend/tursu_admin/urls.py
+++ backend/tursu_admin/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('verificationpending/products/', views.get_products_waiting_verification, name='verification pending products'),
     path('removeproduct/', views.delete_product, name='admin removing products'),
-    path('verifyproduct/', views.delete_product, name='admin verifying products'),
+    path('verifyproduct/', views.verify_product, name='admin verifying products'),
 ]
