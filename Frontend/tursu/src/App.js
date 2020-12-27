fe2ed68383ea2149e082d409d226a50256e4b9b0
mcelimli <mehmetcelimli820@gmail.com>
2020-12-27 19:15:32 +0300

    Conflict resolve

--- Frontend/tursu/src/App.js
+++ Frontend/tursu/src/App.js
@@ -6,10 +6,9 @@ import Category from "./Category";
 import Sign from "./Sign/Sign";
 import ProductDetailPage from "./ProductDetailPage";
 import SearchPage from "./Search";
+import VendorProfilePage from "./Profiles/Vendor/VendorProfilePage";
+import CustomerProfilePage from "./Profiles/Customer/CustomerProfilePage";
 import ShoppingCart from "./ShoppingCart"
-import Sidebar from "./Profiles/VendorSidebar";
-import VendorProfilePage from "./Profiles/VendorProfilePage";
-import CustomerProfilePage from "./Profiles/CustomerProfilePage";
 import AdminPanel from "./AdminPanel"
 
 function App() {
