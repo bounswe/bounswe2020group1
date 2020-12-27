c23a36742309eea539dd1280b6f9bead810a5ee8
mcelimli <mehmetcelimli820@gmail.com>
2020-12-27 19:19:18 +0300

    conflicts has resolved in app.js

--- Frontend/tursu/src/App.js
+++ Frontend/tursu/src/App.js
@@ -9,11 +9,11 @@ import SearchPage from "./Search";
 import VendorProfilePage from "./Profiles/Vendor/VendorProfilePage";
 import CustomerProfilePage from "./Profiles/Customer/CustomerProfilePage";
 import ShoppingCart from "./ShoppingCart"
-
-
-
-
-
+import ShoppingList from "./ShoppingList";
+import AddProduct from "./Profiles/Vendor/AddProduct";
+import Checkout from "./Checkout";
+import EditProduct from "./Profiles/Vendor/EditProduct";
+import DeleteProduct from "./Profiles/Vendor/DeleteProduct";
 import AdminPanel from "./AdminPanel"
 
 function App() {
@@ -28,11 +28,11 @@ function App() {
                 <Route path="/search/:search_string/:search_type/:fprice_lower?/:fprice_upper?/:fvendor_name?/:fcategory?/:sort_by?"  component={SearchPage} />
                 <Route path="/vendorProfile" exact component={VendorProfilePage} />
                 <Route path="/customerProfile" exact component={CustomerProfilePage} />
-
-
-
-
-
+                <Route path="/shoppingList/:name" exact component={ShoppingList} />
+                <Route path="/addProduct" exact component={AddProduct} />
+                <Route path="/editProduct" exact component={EditProduct} />
+                <Route path="/deleteProduct" exact component={DeleteProduct} />
+                <Route path="/checkout" exact component={Checkout} />
                 <Route path="/admin" exact component={AdminPanel} />
             </div>
         </Router>
