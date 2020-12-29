3563aa2ed82af81c640309137240dad3968db6ce
Eray Sezgin <sezgineray@gmail.com>
2020-12-29 11:22:29 +0300

    Added logout functionality.

--- Android/TursuApp/app/src/main/java/com/example/tursuapp/authentication/homepage/ui/profile/ProfileFragment.kt
+++ Android/TursuApp/app/src/main/java/com/example/tursuapp/authentication/homepage/ui/profile/ProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.example.tursuapp.authentication.homepage.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.core.view.GravityCompat
@@ -12,6 +14,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.tursuapp.R
 import com.example.tursuapp.authentication.homepage.HomePageActivity
 import com.example.tursuapp.authentication.homepage.ui.order.CustomerOrdersFragment
+import com.example.tursuapp.authentication.login.LoginActivity
 
 class ProfileFragment : Fragment() {
 
@@ -25,6 +28,25 @@ class ProfileFragment : Fragment() {
         galleryViewModel =
                 ViewModelProvider(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_customer_profile, container, false)
+
+        val button: Button = root!!.findViewById(R.id.logout_button) as Button
+        button.setOnClickListener {
+            val pref = activity?.applicationContext?.getSharedPreferences("UserPref", 0)
+            if (pref != null) {
+                with(pref.edit()) {
+                    remove("first_name")
+                    remove("last_name")
+                    remove("user_type")
+                    remove("auth_token")
+                    putBoolean("logged_in", false)
+                    apply()
+                }
+            }
+            val intent = Intent(activity?.applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+
+        }
 
         return root
     }
