e1e6f940dc8c90ff237ad2054c095544d98bb563
Murat Ekici <muratekici@Murats-MacBook-Pro.local>
2020-12-29 12:06:37 +0300

    shopping cart test fix

--- backend/shopping_cart/tests.py
+++ backend/shopping_cart/tests.py
@@ -14,30 +14,30 @@ class ShoppingCartViewTests(TestCase):
         """Setting up test cases"""
         self.client = APIClient()
         customer = {
-            'email': 'dummy@dummy.com',
-            'username': 'dummy',
+            'email': 'dummy_cart@dummy.com',
+            'username': 'dummy_cart',
             'dateJoined': datetime.datetime(2020,10,19,11,15,24,339437,),
-            'name': 'Dummy',
-            'surname': 'Dummyson',
+            'name': 'Brilliant',
+            'surname': 'Cartson',
             'totalAmountOfMoneySpent': 0,
             }
         vendors = [{
-            'email': 'sellers@apple.com',
-            'username': 'apple',
+            'email': 'sellers@armut.com',
+            'username': 'armut',
             'dateJoined': datetime.datetime(2020,11,19,11,15,24,339437,),
-            'name': 'Apple',
+            'name': 'Armut',
             'surname': None,
             'totalAmountOfMoneySpent': 0,
             'is_verified': True,
-            'location': 'Zincirlikuyu, Istanbul, Turkey',
-            'iban': 'TR730006271333541288459635',
+            'location': 'Kadikoy, Istanbul, Turkey',
+            'iban': 'TR370006271333541288459635',
             'rating ': None,
             }]
         categories = ['Electronics']
         products = [
             {
-                'name': 'Apple iPhone 11',
-                'description': 'Apple iPhone 11 64GB Smart Phone',
+                'name': 'Armut iPhone 11',
+                'description': 'Armut iPhone 11 64GB Smart Phone',
                 'photo_url': 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/\
                     iphone11-black-select-2019_GEO_EMEA?wid=940&hei=1112&fmt=png-alpha&qlt=80&.\
                     v=1567021766023',
@@ -48,10 +48,11 @@ class ShoppingCartViewTests(TestCase):
                 'stock': 1000,
                 'price': 7999.99,
                 'dateAdded': datetime.datetime(2020,11,19,11,15,24,339457,),
+                'is_verified': True,
                 },
             {
-                'name': 'Apple iPad 8. Generation',
-                'description': 'Apple iPad 8. Generation 32 GB 10.2 WiFi Cellular Tablet',
+                'name': 'Armut iPad 8. Generation',
+                'description': 'Armut iPad 8. Generation 32 GB 10.2 WiFi Cellular Tablet',
                 'photo_url': 'https://productimages.hepsiburada.net/s/43/1100/10761074442290.jpg',
                 'vendor': 1,
                 'category': 'Electronics',
@@ -60,10 +61,11 @@ class ShoppingCartViewTests(TestCase):
                 'stock': 1000,
                 'price': 4299.0,
                 'dateAdded': datetime.datetime(2020,11,19,11,18,0,796903,),
+                'is_verified': True,
                 },
             {
-                'name': 'Apple MacBook Air',
-                'description': '- Apple MacBook Air Intel Core i5 5350U 8GB 128GB SSD MacOS \
+                'name': 'Armut MacBook Air',
+                'description': '- Armut MacBook Air Intel Core i5 5350U 8GB 128GB SSD MacOS \
                     Sierra 13.3 inch Laptop Computer',
                 'photo_url': 'https://productimages.hepsiburada.net/s/32/550/10352483860530.jpg',
                 'vendor': 1,
@@ -73,6 +75,7 @@ class ShoppingCartViewTests(TestCase):
                 'stock': 1000,
                 'price': 7485.02,
                 'dateAdded': datetime.datetime(2020,11,19,11,21,17,74807,),
+                'is_verified': True,
                 },
             ]
         for vendor in vendors:
@@ -90,7 +93,7 @@ class ShoppingCartViewTests(TestCase):
         for cat in categories:
             category = Category.objects.create(name=cat)
             category.save()
-        vendor_names = ['Apple']
+        vendor_names = ['Armut']
         for product in products:
             vendor = Vendor.objects.get(user__user__first_name=vendor_names[product['vendor'] - 1])
             category = Category.objects.get(name=product['category'])
@@ -103,6 +106,7 @@ class ShoppingCartViewTests(TestCase):
                 stock=product['stock'],
                 price=product['price'],
                 date_added=product['dateAdded'],
+                is_verified=product['is_verified']
                 )
             prod.save()
         self.user = User.objects.create_user(username=customer['username'],
@@ -119,7 +123,7 @@ class ShoppingCartViewTests(TestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
         product = Product.objects.all()[0]
         product_id = product.id
-        response = self.client.post('/shoppingcart/add/',
+        response = self.client.post('/shoppingcart/add',
             {'quantity': 5, 'product_id': product_id})
         self.assertEqual(response.status_code, 200)
        
@@ -128,10 +132,10 @@ class ShoppingCartViewTests(TestCase):
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
         product = Product.objects.all()[0]
         product_id = product.id
-        response = self.client.post('/shoppingcart/add/',
+        response = self.client.post('/shoppingcart/add',
             {'quantity': 5, 'product_id': product_id})
         self.assertEqual(response.status_code, 200)
-        response = self.client.delete('/shoppingcart/delete/',
+        response = self.client.delete('/shoppingcart/delete',
             {'product_id': product_id})
         self.assertEqual(response.status_code, 200)
 
@@ -142,20 +146,21 @@ class ShoppingCartViewTests(TestCase):
         for i in range(2):
             product = Product.objects.all()[i]
             product_ids.append(product.id)
-            response = self.client.post('/shoppingcart/add/',
-            {'quantity': i + 1, 'product_id': i})
+            response = self.client.post('/shoppingcart/add', {'quantity': i + 1, 'product_id': product.id})
             self.assertEqual(response.status_code, 200)
         
-        response = self.client.get('/shoppingcart/all/')
+        response = self.client.get('/shoppingcart/all')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()[0]["product"]["id"], product_ids[0])
+        self.assertEqual(response.json()[0]["quantity"], 1)
         self.assertEqual(response.json()[1]["product"]["id"], product_ids[1])
+        self.assertEqual(response.json()[1]["quantity"], 2)
 
     def test_missing_params(self):
         """Test case for missing parameter handling"""
         product_id = Product.objects.all()[0].id
         self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token}')
-        response = self.client.post('/shoppingcart/add/')
+        response = self.client.post('/shoppingcart/add')
         self.assertEqual(response.status_code, 400)
-        response = self.client.delete('/shoppingcart/delete/')
+        response = self.client.delete('/shoppingcart/delete')
         self.assertEqual(response.status_code, 400)
