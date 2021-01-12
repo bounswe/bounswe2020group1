77847305902adc6b3683bff0fa0d26b51b01c184
Onur Kılıçoğlu <onurkilic2301@gmail.com>
2021-01-12 17:32:05 +0300

    Activity stream template setting added

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Setting activity stream template"
+BASE_TEMPLATE="/usr/local/lib/python3.9/site-packages/actstream/templates/actstream/base.html"
+ACTOR_TEMPLATE="/usr/local/lib/python3.9/site-packages/actstream/templates/actstream/actor.html"
+cat $ACTOR_TEMPLATE > $BASE_TEMPLATE
+sed -i '1d' $BASE_TEMPLATE
+
 echo "Apply initial migrations"
 python manage.py migrate
 
