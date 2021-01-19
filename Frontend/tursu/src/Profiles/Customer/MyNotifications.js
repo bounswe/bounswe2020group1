5dd067cc9968bbb36b16484e712d4aa99724aa33
Barış ALHAN <barisalhan003@gmail.com>
2021-01-20 00:57:11 +0300

    Data in MyNotifications page is connected to backend and the style is updated.

--- Frontend/tursu/src/Profiles/Customer/MyNotifications.js
+++ Frontend/tursu/src/Profiles/Customer/MyNotifications.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import axios from "axios";
 
 
 
@@ -14,8 +15,8 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(0, 3),
     },
     paper: {
-        maxWidth: 400,
-        marginLeft: '25%',
+        maxWidth: 700,
+        marginLeft: '15%',
         padding: theme.spacing(2),
         marginTop: '30px'
     },
@@ -25,14 +26,59 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-// mock notification data to display
-// TODO: connect with the notification endpoint of backend
-const notifications = ['Your order has been shipped!', 'An item from your wishlist is back on stocks!',
-    'Discount on a product that is on your wishlist!', 'Your order has been shipped!', 'You have a new message!'];
 
 // map every notificiation to display them, like a for loop
 export default function MyNotifications() {
     const classes = useStyles();
+
+    const [notifications, setNotifications] = React.useState([]);
+
+    useEffect(() => {
+        axios.get("http://3.232.20.250/notifications/get_notifications",{
+            headers: {
+                'Authorization': "Token " + window.sessionStorage.getItem("authToken")
+            }
+        }).then(res =>{
+            fillNotifications(res.data)
+            console.log("NOTIFICATIONS")
+            console.log(res.data)
+        })
+    }, [])
+    function fillNotifications(data)
+    {
+        let newList = []
+        for(const item of data)
+        {
+            let text = "";
+            switch (item.type) {
+                case 1:
+                    text += "Your order " + item.product_name + " is in delivery now."
+                    newList.push(text)
+                    break;
+                case 2:
+                    text += "Your product " + item.product_name + " is verified now."
+                    newList.push(text)
+                    break;
+                case 3:
+                    text += "Price Drop: " + item.product_name + " is " + item.new_value + "₺ now."
+                    newList.push(text)
+                    break;
+                case 4:
+                    text += "Price Change: " + item.product_name + " is " + item.new_value + "₺ now."
+                    newList.push(text)
+                    break;
+                case 5:
+                    text += "Stock Change: " + item.product_name + " has " + item.new_value + "products in the stock now."
+                    newList.push(text)
+                    break;
+                default:
+                    console.log(item.type)
+                    break;
+            }
+        }
+        setNotifications(newList)
+    }
+
     return (
         <div className={classes.root}>
 
