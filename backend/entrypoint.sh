b682f59efbb18b5c02e423e83714bf933e08d8f0
Onur Kılıçoğlu <onurkilic2301@gmail.com>
2021-01-20 21:44:58 +0300

    Removed migrations to allow database dump restore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,8 +6,8 @@ ACTOR_TEMPLATE="/usr/local/lib/python3.9/site-packages/actstream/templates/actst
 cat $ACTOR_TEMPLATE > $BASE_TEMPLATE
 sed -i '1d' $BASE_TEMPLATE
 
-echo "Apply initial migrations"
-python manage.py migrate
+#echo "Apply initial migrations"
+#python manage.py migrate
 
 echo "Start the server"
 python manage.py runserver 0.0.0.0:8000
