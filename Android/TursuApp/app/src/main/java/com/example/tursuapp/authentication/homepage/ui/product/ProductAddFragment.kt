93aeb0e355be5f9e0161fe6acb3f7d23855015e3
Ali Batir <ali.batir@boun.edu.tr>
2021-01-24 20:59:42 +0300

    added Unit Tests for the add product

--- Android/TursuApp/app/src/main/java/com/example/tursuapp/authentication/homepage/ui/product/ProductAddFragment.kt
+++ Android/TursuApp/app/src/main/java/com/example/tursuapp/authentication/homepage/ui/product/ProductAddFragment.kt
@@ -144,10 +144,10 @@ class ProductAddFragment: Fragment() {
             if (name.isEmpty() || brand.isEmpty() || stock.isEmpty() || price.isEmpty() || description.isEmpty()) {
                 Toast.makeText(activity?.applicationContext, "Input all product details ", Toast.LENGTH_SHORT).show()
             } else {
-                if (price.toFloat() < 0) {
+                if (!priceInputCheck(price.toFloat())) {
                     Toast.makeText(activity?.applicationContext, "Price must be bigger than zero", Toast.LENGTH_SHORT).show()
                 } else {
-                    if (stock.toInt() < 0) {
+                    if (!stockInputCheck(stock.toInt())) {
                         Toast.makeText(activity?.applicationContext, "Stock must be bigger than zero", Toast.LENGTH_SHORT).show()
                     } else {
                         if (!Uri.EMPTY.equals(image_uri)) {
@@ -254,6 +254,26 @@ class ProductAddFragment: Fragment() {
             }
         }else {
             Toast.makeText(activity?.applicationContext, "Select a category", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun priceInputCheck(price:Float):Boolean{
+        try {
+            if(price < 0){
+                return false
+            }
+            return true
+        }catch (e: Exception){
+            return false
+        }
+    }
+    fun stockInputCheck(stock:Int):Boolean{
+        try {
+            if (stock < 0) {
+                return false
+            }
+            return true
+        }catch (e: Exception){
+            return false
         }
     }
     private val pickImageListener = View.OnClickListener { view ->
